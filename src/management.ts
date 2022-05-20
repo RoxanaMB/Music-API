ee71780a1a84cfc28ee562af2d1e1f850fea0cfb
Gabriel A. Luis Freitas <alu0101348421@ull.edu.es>
2022-05-20 23:59:49 +0000

    Creando test Colaborativo

--- src/management.ts
+++ src/management.ts
@@ -4,6 +4,7 @@ import {postRouter} from './routers/post';
 import {getRouter} from './routers/get';
 import {deleteRouter} from './routers/delete';
 import {defaultRouter} from './routers/default';
+import {patchRouter} from './routers/patch';
 
 const app = express();
 
@@ -11,10 +12,10 @@ app.use(express.json());
 app.use(postRouter);
 app.use(getRouter);
 app.use(deleteRouter);
+app.use(patchRouter);
 app.use(defaultRouter);
 
 const port = process.env.PORT || 3000;
-
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
