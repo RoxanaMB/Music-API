f82e4533817b1c393948de37aa92d3ee4fcde320
Gabriel A. Luis Freitas <alu0101348421@ull.edu.es>
2022-05-20 22:13:27 +0000

    Testing

--- src/routers/post.ts
+++ src/routers/post.ts
@@ -7,79 +7,31 @@ import {Song} from '../models/song';
 export const postRouter = express.Router();
 
 postRouter.post('/artist', async (req, res) => {
-  const artist = new Artist(req.body);
-  let nameFound = false;
-
   try {
+    const artist = new Artist(req.body);
     await artist.save();
     res.status(201).send(artist);
   } catch (err) {
-    if (nameFound) {
-      res.status(409).send({
-        error: 'This artist already exists',
-      });
-    } else {
-      res.status(400).send(err);
-    }
+    res.status(400).send(err);
   }
 });
 
 postRouter.post('/playlist', async (req, res) => {
-  const playlist = new Playlist(req.body);
-  const nameOfBody = req.body.name.toString();
-  const filter = req.body.name?{name: playlist.name.toString()}:{};
-  const findName = Playlist.find(filter);
-  let nameFound = false;
-  findName.then((found) => {
-    found.forEach((element) => {
-      if (element.name === nameOfBody) {
-        nameFound = true;
-      }
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-
   try {
+    const playlist = new Playlist(req.body);
     await playlist.save();
     res.status(201).send(playlist);
   } catch (err) {
-    if (nameFound) {
-      res.status(409).send({
-        error: 'This playlist already exists',
-      });
-    } else {
-      res.status(400).send(err);
-    }
+    res.status(400).send(err);
   }
 });
 
 postRouter.post('/song', async (req, res) => {
-  const song = new Song(req.body);
-  const nameOfBody = req.body.name.toString();
-  const filter = req.body.name?{name: song.title.toString()}:{};
-  const findName = Song.find(filter);
-  let nameFound = false;
-  findName.then((found) => {
-    found.forEach((element) => {
-      if (element.title === nameOfBody) {
-        nameFound = true;
-      }
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-
   try {
+    const song = new Song(req.body);
     await song.save();
     res.status(201).send(song);
   } catch (err) {
-    if (nameFound) {
-      res.status(409).send({
-        error: 'This song already exists',
-      });
-    } else {
-      res.status(400).send(err);
-    }
+    res.status(400).send(err);
   }
 });
