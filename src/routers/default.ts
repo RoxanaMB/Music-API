7a718e73f744ef44b16a7d7a47a29ce90dfdd39d
Roxana Mihaela Baba <58183963+alu0101339887@users.noreply.github.com>
2022-05-21 18:50:01 +0100

    Actualizando routers

--- src/routers/default.ts
+++ src/routers/default.ts
@@ -2,6 +2,10 @@ import * as express from 'express';
 
 export const defaultRouter = express.Router();
 
+/**
+ * It sets a default route to our express app that informs the user that, 
+ * he has hit a wrong route.
+ */
 defaultRouter.all('*', (_, res) => {
   res.status(501).send();
 });
