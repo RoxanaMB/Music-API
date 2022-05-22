1ba1a45a7250e5ea9eb37789508b8f5189e6617d
Gabriel A. Luis Freitas <alu0101348421@ull.edu.es>
2022-05-22 13:25:46 +0000

    Comentarios de mongoose y management realizados

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,17 +1,27 @@
 import {connect, connection} from 'mongoose';
 
-console.log('Connecting to MongoDB...');
-
+/**
+ * URL de la base de datos
+ *  Se toma de la variable de entorno si existe
+ *  Si no, se toma la dirección de una base de datos local por defecto
+ */
 const databaseURL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/app';
 
+/**
+ * Conexión con la base de datos
+ * options:
+ * - useNewUrlParser: true
+ * - useUnifiedTopology: true
+ * - useCreateIndex: true
+ * - useFindAndModify: false
+ */
 connect(databaseURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 }).then(() => {
-  console.log('Connection to MongoDB server established');
-  console.log(databaseURL);
+  console.log(`Connected to ${databaseURL}`);
 }).catch((err) => {
   console.log(err);
   connection.close();
--- src/management.ts
+++ src/management.ts
@@ -6,8 +6,19 @@ import {deleteRouter} from './routers/delete';
 import {defaultRouter} from './routers/default';
 import {patchRouter} from './routers/patch';
 
+/**
+ * Crea una instancia de express
+ */
 const app = express();
 
+/**
+ * Routers:
+ * - defaultRouter: para las rutas desconocidas
+ * - postRouter: para las peticiones POST
+ * - getRouter: para las peticiones GET
+ * - deleteRouter: para las peticiones DELETE
+ * - patchRouter: para las peticiones PATCH
+ */
 app.use(express.json());
 app.use(postRouter);
 app.use(getRouter);
@@ -15,8 +26,16 @@ app.use(deleteRouter);
 app.use(patchRouter);
 app.use(defaultRouter);
 
+/**
+ * Puerto de la aplicación
+ * Se toma de la variable de entorno si existe
+ * Si no, se toma el puerto 3000
+ */
 const port = process.env.PORT || 3000;
 
+/**
+ * Inicia la aplicación en el puerto definido
+ */
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
